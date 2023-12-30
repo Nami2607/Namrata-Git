5e2f21cbd32dc682c3f7de43ce3b88bc8c4e7871
Nami2607 <namrata0726@gmail.com>
2023-12-30 16:53:48 +0530

    active users

---
+++ Active-Users.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+#Prompt user to insert inputs (one at a time) 
+read -p 'Enter Number 1: ' number1 
+read -p 'Enter Number 2: ' number2
+
+# Firstly Validate if any input field is left blank. If not, display appropriate message and stop execution of script 
+if [ -z "$number1" ] || [ -z "$number2" ] 
+then 
+    echo 'Inputs cannot be blank please try again' 
+    exit 0 
+fi
+
+# Now Validate if the user input is a number using regex (Integer or Float). If not, display appropriate message and stop execution of script 
+if ! [[ "$number1" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]] || ! [[ "$number2" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]] 
+then 
+    echo "Inputs must be a numbers" 
+    exit 0 
+fi
+
+# Ok now we are sure that user has provided valid data. Now add the two numbers
+total=$(echo "${number1} + ${number2}"| bc) 
+echo 'Summation Result '$total
